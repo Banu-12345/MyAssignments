e6db5d03a9846daab2eb9dc7d714371b7d48d8fe
Banu-12345 <126339752+Banu-12345@users.noreply.github.com>
2023-02-28 12:35:06 +0530

    Add files via upload

---
+++ day2/LoopName.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class LoopName {
+	public static void main(String[] args) {
+		String str="Banu";
+		for(int i=1;i<=5;i++)
+			System.out.println(str);
+			
+	}
+
+}
---
+++ day2/Palindrome.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=1212;
+
+int num1=num;
+int rev=0;
+for(;num>0;num=num/10) {
+rev=rev*10+num%10;
+//num=num/10;
+}
+System.out.println(rev);
+if(num1==rev)
+{
+System.out.println("it is a palimdrom");
+}
+else
+	System.out.println("it is not a palindrome");
+}
+}
+
