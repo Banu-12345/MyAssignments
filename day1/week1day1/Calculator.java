418152b1163e905d9876869158a83f9c39a31505
Banu-12345 <126339752+Banu-12345@users.noreply.github.com>
2023-03-06 23:33:32 +0530

    Add files via upload

---
+++ day1/week1day1/Calculator.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int a,b;
+		a=20;
+		b=5;
+		double result1, result2;
+		result1=a-b;
+		result2=a/b;
+		System.out.println("subtraction is: " +result1);
+		System.out.println("Division is: "  +result2);
+		
+	}
+
+}
---
+++ day1/week1day1/Mobile.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Mobile {
+public void sendSms()
+{
+boolean isFullCharged;
+int mobileCost;
+System.out.println("This is my mobile");
+	}
+public void makeCall()
+{
+	String mobileModel;
+	Float mobileWeight;
+	System.out.println("i am making call");
+	}
+public void takeSnapShot() 
+{
+System.out.println("How are you");
+}
+public static void main(String[] args) {
+	Mobile mob=new Mobile();
+	mob.sendSms();
+//mob.makeCall();
+}
+}
