d5cbc8f5ab1726c303f5e60058eb16b331f9119d
Banu-12345 <126339752+Banu-12345@users.noreply.github.com>
2023-03-12 12:51:26 +0530

    Add files via upload

---
+++ system/Desktop.java
@@ -0,0 +1,16 @@
+package org.system;
+
+public class Desktop extends Computer {
+	public static void main(String[] args) {
+		
+    Desktop desk=new Desktop();
+	desk.computerModel();
+	desk.desktopSize();
+	}
+	public void desktopSize()
+	{
+		System.out.println("My sisze is 20 inch");
+	}
+}
+
+
